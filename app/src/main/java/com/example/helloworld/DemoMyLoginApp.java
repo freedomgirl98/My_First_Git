ad98658c02094fe3be327c6929752adfe1fa44c1
LEK QIU RONG <19036308@myrp.edu.sg>
2020-04-30 12:51:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/DemoMyLoginApp.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DemoMyLoginApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_my_login_app);
+    }
+}
